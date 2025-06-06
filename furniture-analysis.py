5fe54b24ff2cc09d09f6b8170bb5a17185da305d
Shivtej2404 <shivtej0907@gmail.com>
2025-06-06 11:25:51 +0530

    modified

--- furniture-analysis.py
+++ furniture-analysis.py
@@ -1,4 +1,4 @@
-# Data Collection
+# 1]Data Collection
 import pandas as pd 
 
 # Load Datasets 
@@ -11,3 +11,42 @@ print(furniture.info())
 # Viewing first 5 rows
 print("First 5 rows of the dataset:")
 print(furniture.head())
+
+# 2] Data Cleaning 
+
+# Check for missing values
+print("\nMissing values:")
+print(furniture.isnull().sum())
+
+# Check for duplicates
+print("\nDuplicate values:")
+print(furniture.duplicated().sum())
+
+# Drop duplicates
+furniture.drop_duplicates(inplace=True)
+
+# Drop missing values
+furniture.dropna(inplace=True)
+
+# Drop 'originalPrice' column if it has too many missing values
+furniture.drop(columns=['originalPrice'], inplace=True)
+
+# Clean the 'price' column (remove $ and commas)
+furniture['price'] = furniture['price'].replace('[\$,]', '', regex=True).astype(float)
+
+# Clean the 'tagText' column (categorize values)
+furniture['tagText'] = furniture['tagText'].apply(lambda x: x if x in ['Free shipping', '+Shipping: $5.09'] else 'others')
+
+# Encode 'tagText'
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+furniture['tagText'] = le.fit_transform(furniture['tagText'])
+
+# Final check
+print(furniture.head())
+
+
+# 3] Exploratory Data Analysis
+
+import matplotlib.pyplot as plt
+import seaborn as sns
